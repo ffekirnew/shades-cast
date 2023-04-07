8fdea0d8fcd22480a77d2693d94a64481eaa7d62
Fkrnew Brhanu <ffekirnew0808@gmail.com>
2023-04-07 23:40:52 +0300

    Deleted unneccessary and leaky files

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "api"
-urlpatterns = [
-    path('', views.api_home, name='home'),
-]
--- backend/podcast/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Podcast(models.Model):
-    name = models.CharField(max_length=20)
-    url = models.CharField(max_length=20)
-    description = models.CharField(null=True, blank=True)
