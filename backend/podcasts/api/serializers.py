eb3f7725fae3ca85483a8bcb89a6956bc61944a8
Biruk-Worku-Molla <bworku1221@gmail.com>
2023-04-21 10:46:21 +0300

    added some changes

--- backend/podcasts/api/serializers.py
+++ backend/podcasts/api/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from ..models import Podcast, Episode
 
+
 class PodcastSerializer(serializers.Serializer):
     class Meta:
         model = Podcast
-        fields = ['id', 'title', 'slug', 'description'] #, 'cover_image', 'category', 'status']
+        # , 'cover_image', 'category', 'status']
+        fields = ['id', 'title', 'slug', 'description']
 
 
 class EpisodeSerializer(serializers.Serializer):
     class Meta:
         model = Episode
-        fields = ['id', 'title', 'podcast', 'publish', 'description'] #, 'cover_image', 'category', 'status']
+        # , 'cover_image', 'category', 'status']
+        fields = ['id', 'title', 'podcast', 'publish', 'description']
