a8c1e01f4f4c6c0777d3ead5a59a40d15cd86acf
ffekirnew <ffekirnew0808@gmail.com>
2023-05-17 18:55:30 +0300

    Added final changes

--- backend/user_accounts/urls.py
+++ backend/user_accounts/urls.py
@@ -12,7 +12,7 @@ app_name = 'user_accounts'
 urlpatterns.extend([
     path('<int:id>/followers/', views.UserFollowersAPIView.as_view(), name='user_followers'),
     path('<int:id>/following/', views.UserFollowingAPIView.as_view(), name='user_following'),
-    path('<int:id>/podcasts/', views.UserPodcastsAPIView.as_view(), name='user_podcasts'),
+    path('<int:id>/podcasts-created/', views.UserPodcastsAPIView.as_view(), name='user_podcasts'),
     path('<int:id>/favorite-podcasts/', views.UserFavoritePodcastsAPIView.as_view(), name='user_favorite_podcasts'),
     path('<int:id>/profile/', views.UserProfileDetailView.as_view(), name='user_profile'),
     path('<int:id>/add-follow/', views.user_add_follow_view, name='user_add_follow'),
