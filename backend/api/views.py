7afcec74bfd4a3e706dafc9cd0924416ee91d91e
ffekirnew <ffekirnew0808@gmail.com>
2023-05-19 15:59:11 +0300

    finalized the full text search

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
 from django.contrib.postgres.search import TrigramSimilarity
 
-from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -8,20 +7,22 @@ from taggit.models import Tag
 from podcasts.models import Episode, Podcast
 from podcasts.serializers import EpisodeSerializer, PodcastSerializer
 
+from .serializers import TagSerializer
+
 
 @api_view(['GET'])
 def search(request, query):
     podcast_results = Podcast.objects.annotate(
-        similarity=TrigramSimilarity('title', query) + TrigramSimilarity('description', query),
-    ).filter(similarity__gt=0.1).order_by('-similarity')
+        similarity=TrigramSimilarity('title', query) + TrigramSimilarity('description', query)
+    ).filter(similarity__gt=0.01).order_by('-similarity')
 
     episode_results = Episode.objects.annotate(
-        similarity=TrigramSimilarity('title', query) + TrigramSimilarity('description', query),
-    ).filter(similarity__gt=0.1).order_by('-similarity')
+        similarity=TrigramSimilarity('title', query) + TrigramSimilarity('description', query)
+    ).filter(similarity__gt=0.01).order_by('-similarity')
 
     tag_results = Tag.objects.annotate(
-        similarity=TrigramSimilarity('name', query),
-    ).filter(similarity__gt=0.1).order_by('-similarity')
+        similarity=TrigramSimilarity('name', query)
+    ).filter(similarity__gt=0.01).order_by('-similarity')
 
     podcasts_serialized = PodcastSerializer(podcast_results, many=True)
     episodes_serialized = EpisodeSerializer(episode_results, many=True)
@@ -34,9 +35,3 @@ def search(request, query):
     }
 
     return Response(results)
-
-
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
