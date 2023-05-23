c062620faab318e3d8b591320df8808ad6869fa9
Shamil Bedru <shamilbedru47@gmail.comet-shamil.bedru>
2023-05-23 23:25:21 +0300

    .

--- mobile/shades_cast/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/shades_cast/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,19 +5,14 @@
 import FlutterMacOS
 import Foundation
 
-// <<<<<<< firaols_branch
-// import audio_session
-// import just_audio
-// import path_provider_foundation
-
-// func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-//   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-//   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
-//   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-// =======
+import audio_session
+import just_audio
+import path_provider_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-// >>>>>>> master
 }
