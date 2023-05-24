80342b68d02c7d29afe9df6e75cb3a2978adc05f
ffekirnew <ffekirnew0808@gmail.com>
2023-05-24 22:27:57 +0300

    Implemented the facts feature

--- backend/facts/urls.py
+++ backend/facts/urls.py
@@ -1,1 +1,10 @@
-from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from . import views
+
+app_name = 'facts'
+
+router = SimpleRouter()
+router.register('', views.FactViewSet, basename='facts')
+
+urlpatterns = router.urls
