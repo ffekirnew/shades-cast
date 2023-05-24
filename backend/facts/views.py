11bc2fafb65fc54b902038b6c4646e5d413a53d0
ffekirnew <ffekirnew0808@gmail.com>
2023-05-24 22:27:57 +0300

    Implemented the facts feature

--- backend/facts/views.py
+++ backend/facts/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from .models import Fact
 from .serializers import FactSerializer
+from .permissions import IsUserAdminOrReadOnly
 
 
 class FactViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsUserAdminOrReadOnly,)
     queryset = Fact.objects.all()
     serializer_class = FactSerializer
-
-# Create your views here.
