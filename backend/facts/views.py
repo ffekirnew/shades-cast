881386b05601779fc4f16e0450c82e76990a7a68
ffekirnew <ffekirnew0808@gmail.com>
2023-05-24 23:10:27 +0300

    Implemented the facts feature

--- backend/facts/views.py
+++ backend/facts/views.py
@@ -1,11 +1,35 @@
-from rest_framework import viewsets
+import random
+
+from django.shortcuts import get_object_or_404
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
 from .models import Fact
 from .serializers import FactSerializer
 from .permissions import IsUserAdminOrReadOnly
 
 
-class FactViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsUserAdminOrReadOnly,)
+@api_view(['GET'])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def get_random_fact(request):
+    count = Fact.objects.count()
+    random_id = random.randint(1, count)
+
+    random_fact = Fact.objects.get(id=random_id)
+    serializer = FactSerializer(random_fact)
+
+    return Response(serializer.data)
+
+
+class FactsListView(ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    queryset = Fact.objects.all()
+    serializer_class = FactSerializer
+
+
+class FactRetrieveUpdateView(RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly, IsUserAdminOrReadOnly, ]
     queryset = Fact.objects.all()
     serializer_class = FactSerializer
