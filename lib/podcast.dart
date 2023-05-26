a314cf6f4f3874290fc41c7b95fb71aed0538f91
kalkidan-hub <kalkidandereje666@gmail.com>
2023-05-26 13:25:38 +0300

    podcast model, to comply with the documentation

--- lib/podcast.dart
+++ lib/podcast.dart
@@ -1,31 +1,48 @@
 class Podcast {
-  int id;
-  String creator;
-  String title;
-  String status;
+  final int id;
+  final String title;
+  final String? author;
+  final String? description;
+  final String? imageUrl;
+  final List? categories;
 
-  Podcast({
-    required this.id,
-    required this.creator,
-    required this.title,
-    required this.status,
-  });
+  Podcast(
+      {required this.id,
+      required this.title,
+      required this.author,
+      required this.description,
+      required this.imageUrl,
+      required this.categories});
 
   factory Podcast.fromJson(Map<String, dynamic> json) {
     return Podcast(
       id: json['id'],
-      creator: json['creator'],
       title: json['title'],
-      status: json['status'],
+      author: json['creator'],
+      description: json['description'],
+      imageUrl: json['cover_image'],
+      categories: json['categories'],
     );
   }
-
-  Map<String, dynamic> toJson() {
+  Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'creator': creator,
       'title': title,
-      'status': status,
+      'description': description,
+      'author': author,
+      'imageUrl': imageUrl,
+      'categories': categories
     };
+  }
+
+  static Podcast fromMap(Map<String, dynamic> map) {
+    return Podcast(
+      id: map['id'],
+      title: map['title'],
+      author: map['creator'],
+      description: map['description'],
+      imageUrl: map['cover_image'],
+      categories: map['categories'],
+    );
   }
 }
