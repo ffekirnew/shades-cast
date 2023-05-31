ef8f722efb237360ca5a102854078ab039fb6c76
ffekirnew <ffekirnew0808@gmail.com>
2023-05-31 22:25:56 +0300

    added some new changes to the database

---
+++ backend/podcasts/test_models.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_podcast(self):
+        self.fail()
+
+    def test_episode(self):
+        self.fail()
+
+    def test_subscription(self):
+        self.fail()
+
+    def test_playback(self):
+        self.fail()
