763ed4404fbe4ddc1de8bf9e1f9988cc85930c68
ffekirnew <ffekirnew0808@gmail.com>
2023-06-02 10:07:13 +0300

    added endpoint for alternative token authentication

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import viewsets, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
